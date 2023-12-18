feaf55e98dc5948f78cbf6280a61c7dc2a362a4f
Kristijan-Vukovic <149951487+Kristijan-Vukovic@users.noreply.github.com>
2023-12-18 19:35:50 +0100

    Update Program.cs

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(7);
+Console.WriteLine(true);
+Console.Write("Prvi");
+Console.Write("Drugi");
+Console.WriteLine(3.14);
